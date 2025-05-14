e95c79b2c179a75c07d61824ede8891ae945e1dd
Supersonic Seven <b.rockholz@icloud.com>
2025-05-14 16:47:49 -0400

    Blud
    Blud

--- src/core/secret-formula/reality/imaginary-upgrades.js
+++ src/core/secret-formula/reality/imaginary-upgrades.js
@@ -326,7 +326,7 @@ export const imaginaryUpgrades = [
     cost: 1e50,
     requirement: () => `Reach ${format(Decimal.NUMBER_MAX_VALUE, 2)} Singularities`,
     hasFailed: () => false,
-    checkRequirement: () => Currency.singularities.gte(Decimal.NUMBER_MAX_VALUE),
+    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15,//Currency.singularities.gte(Decimal.NUMBER_MAX_VALUE),
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     description: "Unlock the 5th Dark Matter Dimension, raise Dark Matter cap to 1e1000",
   },
@@ -335,11 +335,11 @@ export const imaginaryUpgrades = [
     id: 27,
     cost: 1e100,
     requirement: () => `Reach 1e9e15 Antimatter in Pelle without ever equipping Glyphs`,
-    hasFailed: () => !player.celestials.pelle.doomed || Glyphs.activeWithoutCompanion.length > 1,
+    hasFailed: () => false,//!player.celestials.pelle.doomed || Glyphs.activeWithoutCompanion.length > 0,
     // We have to put this as 9.001e15 for now because Glyphs can still be swtiched out via Armageddon
     // Hopefully we can fix this later
-    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15 &&
-      Glyphs.activeWithoutCompanion.length <= 1,
+    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15,// &&
+      //Glyphs.activeWithoutCompanion.length <= 0,
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     description: "Unlock the 6th Dark Matter Dimension, raise Dark Matter cap to 1e4000",
   },
@@ -348,8 +348,8 @@ export const imaginaryUpgrades = [
     id: 28,
     cost: 1e150,
     requirement: () => `Unlock Pelle without completing any Hard V-Achievements`,
-    hasFailed: () => V.spaceTheorems >= 37,
-    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15 && V.spaceTheorems <= 36,
+    hasFailed: () => false,//V.spaceTheorems >= 37,
+    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15,//&& V.spaceTheorems <= 36,
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     description: "Unlock the 7th Dark Matter Dimension, raise Dark Matter cap to 1e20000",
   },
@@ -359,8 +359,8 @@ export const imaginaryUpgrades = [
     cost: 1e200,
     requirement: () => `Have a total of 1e100 Galaxies`,
     hasFailed: () => false,
-    checkRequirement: () => Replicanti.galaxies.total + player.galaxies +
-      player.dilation.totalTachyonGalaxies >= 1e100,
+    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15,//Replicanti.galaxies.total + player.galaxies +
+      //player.dilation.totalTachyonGalaxies >= 1e100,
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     description: "Unlock the 8th Dark Matter Dimension, raise Dark Matter cap to 1e100000",
   },
@@ -369,12 +369,12 @@ export const imaginaryUpgrades = [
     id: 30,
     cost: 1.8e308,
     requirement: () => `Disable all Nerfs and Strikes in Pelle`,
-    hasFailed: () => !player.celestials.pelle.doomed,
-    checkRequirement: () => Currency.antimatter.value.exponent >= 9e115 && player.celestials.pelle.doomed,
+    hasFailed: () => false,//!player.celestials.pelle.doomed,
+    checkRequirement: () => Currency.antimatter.value.exponent >= 9.001e15,//Currency.antimatter.value.exponent >= 9e115 && player.celestials.pelle.doomed,
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
-    description: () => {
-      if (ImaginaryUpgrade(30).isBought) return "Unlock Alpha, Celestial of Darkness";
-      return "Unlock ???, Celestial of ???";
-    },
+    description: "Unlock ???, Celestial of ???",//() => {
+      //if (ImaginaryUpgrade(30).isBought) return "Unlock Alpha, Celestial of Darkness";
+      //return "Unlock ???, Celestial of ???";
+    //},
   },
 ];
