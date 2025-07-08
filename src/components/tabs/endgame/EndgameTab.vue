fee7c26d8f2700646eeda87ecafe29ba752acbff
Supersonic Seven <b.rockholz@icloud.com>
2025-07-08 16:32:45 -0400

    Update EndgameTab.vue
    Endgame file?

--- src/components/tabs/endgame/EndgameTab.vue
+++ src/components/tabs/endgame/EndgameTab.vue
@@ -1,5 +1,27 @@
+<script>
+  endgames() {
+    return player.progress.fullGameCompletions;
+  }
+</script>
+
 <template>
-  <div>
-    Coming Soon...
+  <div class="endgame-nyi">
+    You aren't supposed to be here...
+    <br>
+    UPDATE IN 5 HOURS
+    <br>
+    You have reached Endgame {{ formatInt(endgames) }} times.
   </div>
 </template>
+
+<style>
+  .endgame-nyi {
+    width: 50rem;
+    padding: 0.5rem;
+    font-size: 2.4rem;
+    color: var(--color-endgame);
+    background: black;
+    border: var(--var-border-width, 0.2rem) solid var(--color-endgame);
+    border-radius: var(--var-border-radius, 0.5rem);
+  }
+</style>
