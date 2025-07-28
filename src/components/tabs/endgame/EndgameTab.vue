6cbbc1efd51acb8ba5b8bd822bfce7f81010ce3c
Supersonic Seven <b.rockholz@icloud.com>
2025-07-28 11:40:30 -0400

    Update EndgameTab.vue
    Checklist

--- src/components/tabs/endgame/EndgameTab.vue
+++ src/components/tabs/endgame/EndgameTab.vue
@@ -1,24 +1,75 @@
 <template>
-  <div class="endgame-nyi">
+  <div class="endgame-header">
     <br>
     <div>
-      You aren't supposed to be here...
+      Endgame Checklist:
+    </div>
+  </div>
+  <div class="endgame-text">
+    <div>
+      New Achievement Row: 83%
     </div>
     <br>
     <div>
-      UPDATE IN 05:00:00.
+      Reset Layer: 100%
     </div>
     <br>
     <div>
-      Please wait for further updates...
+      New Quotes: 25%
+    </div>
+    <br>
+    <div>
+      New Imaginary Upgrades: 67%
+    </div>
+    <br>
+    <div>
+      Celestial Dimensions: 0%
+    </div>
+    <br>
+    <div>
+      New Currency: 17%
+    </div>
+    <br>
+    <div>
+      Break Eternity: 0%
+    </div>
+    <br>
+    <div>
+      Break Eternity Repo: ~80% (varies on refactor branch)
+    </div>
+    <br>
+    <div>
+      Pelle Destruction: 0%
+    </div>
+    <br>
+    <div>
+      Endgame Animations: 50%
+    </div>
+    <br>
+    <div>
+      Start Animations: 0%
+    </div>
+  </div>
+  <div class="endgame-header">
+    <br>
+    <div>
+      Total Progress: 38%
+    </div>
+    <br>
+    <div>
+      Estimated Completion: 16 August 2025
     </div>
   </div>
 </template>
 
 <style scoped>
-.endgame-nyi {
+.endgame-header {
   font-size: 2rem;
   font-weight: bold;
+  color: var(--color-endgame);
+}
+.endgame-text {
+  font-size: 2rem;
   color: var(--color-endgame);
 }
 </style>
