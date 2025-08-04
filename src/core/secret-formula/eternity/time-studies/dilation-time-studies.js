075de093dcec2b5119a5f3f367f560343d6838b5
Supersonic Seven <b.rockholz@icloud.com>
2025-08-04 09:09:25 -0400

    Update dilation-time-studies.js
    crash?

--- src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
@@ -43,13 +43,18 @@ export const dilationTimeStudies = [
   },
   {
     id: 6,
-    description: () => (Pelle.isDoomed
-      ? "You cannot escape a Doomed Reality"
-      : "Unlock Reality"),
+    description: () => {
+      if (Pelle.isDoomed) {
+        if (player.celestials.pelle.galaxyGenerator.unlocked) return "*";
+        return "You cannot escape a Doomed Reality";
+      },
+      return "Unlock Reality";
+    },
     cost: 1,
     requirement: () => TimeStudy.timeDimension(8).isBought &&
       player.records.thisReality.maxEP.exponent >= 4000 &&
       (Perk.firstPerk.isBought || Achievements.preReality.every(a => a.isUnlocked)) &&
-      !Pelle.isDoomed
+      !Pelle.isDoomed,
+    scrambleText: ["You cannot escape a Doomed Reality", "Escape the Doomed Reality (Requires e9e15 Antimatter)"],
   }
 ];
