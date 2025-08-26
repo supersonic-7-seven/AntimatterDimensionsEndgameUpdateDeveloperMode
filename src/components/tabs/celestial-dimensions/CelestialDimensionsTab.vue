5d3fa2d76c170129ac678862257a6f8c7edbb2e8
Supersonic Seven <b.rockholz@icloud.com>
2025-08-25 20:59:27 -0400

    Create CelestialDimensionsTab.vue
    Taken all

---
+++ src/components/tabs/celestial-dimensions/CelestialDimensionsTab.vue
@@ -0,0 +1,23 @@
+<script>
+import ClassicCelestialDimensionsTab from "./ClassicCelestialDimensionsTab";
+import ModernCelestialDimensionsTab from "./ModernCelestialDimensionsTab";
+
+export default {
+  name: "CelestialDimensionsTab",
+  components: {
+    ClassicCelestialDimensionsTab,
+    ModernCelestialDimensionsTab
+  },
+  computed: {
+    activeComponent() {
+      return this.$viewModel.newUI
+        ? "ModernCelestialDimensionsTab"
+        : "ClassicCelestialDimensionsTab";
+    }
+  }
+};
+</script>
+
+<template>
+  <component :is="activeComponent" />
+</template>
