399d1f88d912255760c6ca19f881e2cfa099ab3d
Supersonic Seven <b.rockholz@icloud.com>
2025-08-29 19:02:00 -0400

    Update replicanti.js
    Would this work?

--- src/core/replicanti.js
+++ src/core/replicanti.js
@@ -140,10 +140,10 @@ export function totalReplicantiSpeedMult(overCap) {
     Achievement(81),
     TimeStudy(62),
     TimeStudy(213),
-    RealityUpgrade(2),
+    RealityUpgrade(2).effect.toNumber(),
     RealityUpgrade(6),
     RealityUpgrade(23),
-  ).toNumber();
+  );
   totalMult = totalMult.times(preCelestialEffects);
   if (TimeStudy(132).isBought) {
     totalMult = totalMult.times(Perk.studyPassive.isBought ? 3 : 1.5);
