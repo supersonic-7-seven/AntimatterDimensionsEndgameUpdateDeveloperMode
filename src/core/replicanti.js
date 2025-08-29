da373f796ef7efb7062bec521150821412e062ee
Supersonic Seven <b.rockholz@icloud.com>
2025-08-29 19:25:05 -0400

    Update replicanti.js
    Fix

--- src/core/replicanti.js
+++ src/core/replicanti.js
@@ -140,7 +140,7 @@ export function totalReplicantiSpeedMult(overCap) {
     Achievement(81),
     TimeStudy(62),
     TimeStudy(213),
-    RealityUpgrade(2).effect.toNumber(),
+    RealityUpgrade(2),
     RealityUpgrade(6),
     RealityUpgrade(23),
   );
