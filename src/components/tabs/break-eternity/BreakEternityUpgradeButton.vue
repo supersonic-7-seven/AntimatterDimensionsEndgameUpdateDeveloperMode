9d63d71bfbba100ec096f4aad89b445c8358ed61
Supersonic Seven <b.rockholz@icloud.com>
2025-08-28 21:09:26 -0400

    Create BreakEternityUpgradeButton.vue
    Finish file

---
+++ src/components/tabs/break-eternity/BreakEternityUpgradeButton.vue
@@ -0,0 +1,66 @@
+<script>
+import CostDisplay from "@/components/CostDisplay";
+import DescriptionDisplay from "@/components/DescriptionDisplay";
+import EffectDisplay from "@/components/EffectDisplay";
+
+export default {
+  name: "BreakEternityUpgradeButton",
+  components: {
+    DescriptionDisplay,
+    EffectDisplay,
+    CostDisplay
+  },
+  props: {
+    upgrade: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isBought: false,
+      isAffordable: false
+    };
+  },
+  computed: {
+    classObject() {
+      return {
+        "o-break-eternity-upgrade": true,
+        "o-break-eternity-upgrade--bought": this.isBought,
+        "o-break-eternity-upgrade--available": !this.isBought && this.isAffordable,
+        "o-break-eternity-upgrade--unavailable": !this.isBought && !this.isAffordable
+      };
+    }
+  },
+  methods: {
+    update() {
+      const upgrade = this.upgrade;
+      this.isBought = upgrade.isBought;
+      this.isAffordable = upgrade.isAffordable;
+    }
+  }
+};
+</script>
+
+<template>
+  <button
+    :class="classObject"
+    @click="upgrade.purchase()"
+  >
+    <DescriptionDisplay :config="upgrade.config" />
+    <EffectDisplay
+      br
+      :config="upgrade.config"
+    />
+    <CostDisplay
+      v-else-if="!isBought"
+      br
+      :config="upgrade.config"
+      name="Antimatter Exponent"
+    />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
