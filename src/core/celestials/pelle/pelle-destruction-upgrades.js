3827ab5f6b3450e152a2563f7a9f033f87f376ac
Supersonic Seven <b.rockholz@icloud.com>
2025-09-08 18:08:23 -0400

    Create pelle-destruction-upgrades.js
    This should be enough

---
+++ src/core/celestials/pelle/pelle-destruction-upgrades.js
@@ -0,0 +1,24 @@
+export class PelleDestructionUpgradeState extends SetPurchasableMechanicState {
+
+  get set() {
+    return player.endgame.pelleDestruction.upgrades;
+  }
+
+  get currency() {
+    return Currency.doomedParticles;
+  }
+
+  get description() {
+    return this.config.description;
+  }
+
+  get cost() {
+    return this.config.cost;
+  }
+
+}
+
+export const PelleDestructionUpgrade = mapGameDataToObject(
+  GameDatabase.endgame.pelleDestruction.upgrades,
+  config => new PelleDestructionUpgradeState(config)
+);
