d479ea33f665de2e9b89e5138da874b831d01276
Supersonic Seven <b.rockholz@icloud.com>
2025-09-10 13:44:26 -0400

    Update normal-achievements.js
    Fixing Achievement 193 triggering at the wrong time

--- src/core/secret-formula/achievements/normal-achievements.js
+++ src/core/secret-formula/achievements/normal-achievements.js
@@ -1429,7 +1429,7 @@ export const normalAchievements = [
     id: 193,
     name: "Unstoppable",
     description: "Beat Doom in Run 2.",
-    checkRequirement: () => PlayerProgress.endgameUnlocked() && Currency.antimatter.exponent >= 9e15,
+    checkRequirement: () => PlayerProgress.endgameUnlocked() && Currency.antimatter.exponent >= 9e15 && Pelle.isDoomed,
     checkEvent: GAME_EVENT.GAME_TICK_AFTER,
     get reward() {
       return `Galaxies are ${formatPercents(0.1)} stronger.`;
