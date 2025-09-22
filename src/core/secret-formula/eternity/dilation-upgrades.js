7b6e9a4ba54b122787c607308ddacd1ec6564441
Supersonic Seven <b.rockholz@icloud.com>
2025-09-22 15:01:45 -0400

    Update dilation-upgrades.js
    We trying this again

--- src/core/secret-formula/eternity/dilation-upgrades.js
+++ src/core/secret-formula/eternity/dilation-upgrades.js
@@ -14,7 +14,7 @@ function rebuyable(config) {
     effect: () => config.effect(player.dilation.rebuyables[config.id]),
     formatEffect: config.formatEffect,
     formatCost: config.formatCost,
-    purchaseCap: () => config.purchaseCap(player.dilation.rebuyables[config.id]),
+    purchaseCap: config.purchaseCap,
     reachedCap: () => player.dilation.rebuyables[config.id] >= config.purchaseCap,
     pelleOnly: Boolean(config.pelleOnly),
     rebuyable: true
@@ -70,7 +70,8 @@ export const dilationUpgrades = {
         Next: ${formatX(getTachyonGalaxyMult(nextEffect), 4, 4)}`;
     },
     formatCost: value => format(value, 2),
-    purchaseCap: () => (BreakEternityUpgrade.tgThresholdUncap.isBought ? Number.MAX_VALUE : 38)
+    hasImprovedCap: BreakEternityUpgrade.tgThresholdUncap.isBought,
+    purchaseCap: hasImprovedCap ? Number.MAX_VALUE : 38
   }),
   tachyonGain: rebuyable({
     id: 3,
