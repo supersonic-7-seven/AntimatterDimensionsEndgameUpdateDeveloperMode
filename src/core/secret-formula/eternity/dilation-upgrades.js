66077886b86d9f6140ea8fcccad1b5b585b93ecd
Supersonic Seven <b.rockholz@icloud.com>
2025-09-22 14:35:36 -0400

    Update dilation-upgrades.js
    YEAH

--- src/core/secret-formula/eternity/dilation-upgrades.js
+++ src/core/secret-formula/eternity/dilation-upgrades.js
@@ -14,7 +14,7 @@ function rebuyable(config) {
     effect: () => config.effect(player.dilation.rebuyables[config.id]),
     formatEffect: config.formatEffect,
     formatCost: config.formatCost,
-    purchaseCap: () => config.purchaseCap,
+    purchaseCap: () => config.purchaseCap(player.dilation.rebuyables[config.id]),
     reachedCap: () => player.dilation.rebuyables[config.id] >= config.purchaseCap,
     pelleOnly: Boolean(config.pelleOnly),
     rebuyable: true
