61ffad3cd8c606cbdb525017d325e7c8b6d2abc9
Supersonic Seven <b.rockholz@icloud.com>
2025-09-30 17:51:55 -0400

    Update index.js
    Indexed once again

--- src/core/secret-formula/endgame/index.js
+++ src/core/secret-formula/endgame/index.js
@@ -1,7 +1,9 @@
 import { breakEternityUpgrades } from "./break-eternity-upgrades";
 import { endgameMasteries } from "./endgame-masteries";
+import { permanentEndgameMasteries } from "./permanent-endgame-masteries";
 
 export const endgame = {
   upgrades: breakEternityUpgrades,
-  masteries: endgameMasteries
+  masteries: endgameMasteries,
+  permanentMasteries: permanentEndgameMasteries
 };
